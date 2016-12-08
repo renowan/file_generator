9cdedd33a104ea04f02e419dd7c76e5902e9e264
温 穎峰 <renowan@yahoo.co.jp>
2016-12-09 02:18:10 +0900

    作業

--- create/main.js
+++ create/main.js
@@ -2,9 +2,34 @@
 // import chalk from 'chalk'
 // import yosay from 'yosay'
 
-var inquirer = require('inquirer');
+// moduleName
+
+// var inquirer = require('inquirer');
+var memFs = require('mem-fs');
+// var editor = require('mem-fs-editor');
+var editor = require('mem-fs-editor');
+//
+var store = memFs.create();
+var fs = editor.create(store);
+
+fs.read('templates/pageViewIndex.ejs')
+
+// var File = require('vinyl');
+// var store = require('mem-fs').create();
+//
+// var coffeeFile = new File({
+//   cwd: '/',
+//   base: '/test/',
+//   path: '/test/file.coffee',
+//   contents: new Buffer('test = 123')
+// });
+//
+// store.add(coffeeFile);
 
 
+// fs.write('/create/templates/pageViewIndex.ejs', 'var a = 1;');
+
+/*
 inquirer.prompt([
   {
     type: 'list',
@@ -13,12 +38,20 @@ inquirer.prompt([
     choices: ['action', 'pageView', 'component', 'redux'],
     default: 'redux'
   },
-  {
-    type: 'input',
-    name: 'name',
-    message: 'ファイル名',
-    default: 'foo'
-  }
+  // {
+  //   type: 'input',
+  //   name: 'name',
+  //   message: 'ファイル名',
+  //   default: 'foo'
+  // }
 ]).then(function (answers) {
-  console.log(JSON.stringify(answers, null, '  '));
+  // console.log(JSON.stringify(answers, null, '  '));
+
+
+  // fs.copyTpl('/create/templates/pageViewIndex.ejs', '/src/view/index.js', {moduleName: 'fofo'});
+
+  fs.write('somefile.js', 'var a = 1;');
+
 });
+
+*/
