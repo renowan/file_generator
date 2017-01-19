93e4204aa2604acf20546fcf3f60cd4b88f66ad0
温 穎峰 <renowan@yahoo.co.jp>
2017-01-20 02:10:06 +0900

    項目選択 -> ファイル生成、大成功

--- create/main.js
+++ create/main.js
@@ -1,26 +1,29 @@
-import path from 'path'
+// import path from 'path'
 import ejs from 'ejs'
 import fs from 'fs'
+import inquirer from 'inquirer'
 
-var dir = './tmp';
+// var dir = './tmp';
+//
+// if (!fs.existsSync(dir)){
+//     fs.mkdirSync(dir);
+// }
+//
+// var hello = fs.readFileSync('./create/templates/pageViewIndex.js', 'utf8');
+//
+// var hello2 = ejs.render(hello, {
+//     moduleName:"myMyMy"
+// });
+//
+// fs.writeFile(dir + "/a.js", hello2, function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//     console.log("The file was saved!");
+// });
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
 
-var hello = fs.readFileSync('./create/templates/pageViewIndex.js', 'utf8');
-
-var hello2 = ejs.render(hello, {
-    moduleName:"myMyMy"
-});
-
-fs.writeFile(dir + "/a.js", hello2, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});
-
+// ↑　完成
 
 // var coffeeFile = new File({
 //   cwd: '/',
@@ -34,7 +37,7 @@ fs.writeFile(dir + "/a.js", hello2, function(err) {
 
 // fs.write('/create/templates/pageViewIndex.ejs', 'var a = 1;');
 
-/*
+
 inquirer.prompt([
   {
     type: 'list',
@@ -43,20 +46,43 @@ inquirer.prompt([
     choices: ['action', 'pageView', 'component', 'redux'],
     default: 'redux'
   },
-  // {
-  //   type: 'input',
-  //   name: 'name',
-  //   message: 'ファイル名',
-  //   default: 'foo'
-  // }
+  {
+    type: 'input',
+    name: 'name',
+    message: 'ファイル名',
+    default: 'foo'
+  }
 ]).then(function (answers) {
-  // console.log(JSON.stringify(answers, null, '  '));
+
+  const kind = answers.kind
+  const name = answers.name
+
+  const tplCollection = {
+    'action'   : 'action.js',
+    'pageView' : 'pageView.js',
+    'component': 'component.js',
+    'redux'    : 'redux.js',
+  }
+
+  const dir = `./dist/${kind}/`
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  }
+  var tpl = fs.readFileSync('./create/templates/' + tplCollection[kind] , 'utf8');
+  var fileSource = ejs.render(tpl, {
+      moduleName: name,
+      upperName: name,
+      name: name
+  });
+  fs.writeFile(dir + "/" + tplCollection[kind] , fileSource, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  });
 
 
-  // fs.copyTpl('/create/templates/pageViewIndex.ejs', '/src/view/index.js', {moduleName: 'fofo'});
-
-  fs.write('somefile.js', 'var a = 1;');
+  // console.log(answers);
+  // fs.write('somefile.js', 'var a = 1;');
 
 });
-
-*/
--- create/templates/pageViewIndex.ejs.js
+++
@@ -1,2 +0,0 @@
-import <%= moduleName %>View from './<%= moduleName %>View'
-export default <%= moduleName %>View
